ee99580862fd62969a132b956fd2dc91ce8218fc
henery <Henery_002@163.com>
2026-02-02 22:23:30 +0800

    Initial Commit

---
+++ ClipLikeTests/ClipLikeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipLikeTests.swift
+//  ClipLikeTests
+//
+//  Created by henery on 2026/2/2.
+//
+
+import Testing
+@testable import ClipLike
+
+struct ClipLikeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
